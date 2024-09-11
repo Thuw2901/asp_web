00d9a452bf2f9e4a02bc94e4befa9349dd9c5a49
Thuw2901 <Nguyendangminhthu2004@gmail.com>
2024-09-11 23:08:54 +0700

    BS

---
+++ BaiTapKiemTra01/Models/SanPhamViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BaiTapKiemTra01.Models
+
+{
+    public class SanPhamViewModel
+    {
+        public string TenSP { get; set; }
+        public decimal GiaBan { get; set; }
+        public string AnhMoTa { get; set; }
+        
+    }
+}
